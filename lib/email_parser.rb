73aca7139c227a54ec706b17ac2336258afb04bf
Bruce Parker <detailsharks@gmail.com>
2020-06-21 19:30:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,24 +3,4 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailParser
 
-  attr_accessor :emails
-
-  def initialize(emails)
-    @emails = emails
-  end
-
-  def parse
-    email_array = []
-    email_array = @emails.split(/[,\s]/)
-    email_array = email_array.reject { |e| e == ""}
-    email_array.uniq
-  end
-
-end
-
-emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
-parser = EmailParser.new(emails)
-
-puts parser.parse
