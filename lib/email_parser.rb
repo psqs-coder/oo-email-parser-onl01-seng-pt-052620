7fbdf471ee641e34e3e2f1b8574d12f513dfa892
Bruce Parker <detailsharks@gmail.com>
2020-06-21 19:22:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,11 +13,4 @@ def initialize(emails)
 end 
 
 def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
-
-end
-
-
+  puts 
